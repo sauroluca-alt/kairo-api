c7ae84270c9abf3caf11e6f41b509a9b3e499260
Luca <luca.sauro@kairoapp.es>
2026-04-21 16:38:52 +0200

    Use bcryptjs instead of bcrypt

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,6 +1,6 @@
 import type { FastifyPluginAsync } from 'fastify'
 import { z } from 'zod'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import { v4 as uuid } from 'uuid'
 
 // ── SCHEMAS ────────────────────────────────────────────────────────────────────
